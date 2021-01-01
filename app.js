3922a2bb7b8d5e23e96735cdad4feb33b05bf3d0
Dani-elaqh <ale.quintanillah@gmail.com>
2021-01-01 21:18:10 +0000

    app js

--- app.js
+++ app.js
@@ -13,6 +13,30 @@ router.use(express.static(path.resolve(__dirname, 'views'))); //We define the vi
 router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
 router.use(express.json()); //We include support for JSON that is coming from the client
 
+// Function to read in XML file and convert it to JSON
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}
+
+//Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(obj);
+  fs.unlinkSync(filepath);
+  fs.writeFile(filepath, xml, cb);
+}
+
+router.get('/', function(req, res) {
+
+    res.render('index');
+
+});
+
 
 router.get('/get/html', function(req, res) {
 
